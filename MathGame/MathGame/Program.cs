6be9ef232b0539469711e6304e0a13398c98b1cf
Alphin poly <106670794+alphaleo21@users.noreply.github.com>
2024-08-21 12:28:17 +0530

    created welcome and menu method

---
+++ MathGame/MathGame/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace MathGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome();
+            gameMenu();
+
+        }
+
+        private static void gameMenu()
+        {
+            var index = 1;
+            Console.WriteLine("Select the game you want to play\n");
+            Console.WriteLine($"{index++}.Addition");
+            Console.WriteLine($"{index++}.Subtraction");
+            Console.WriteLine($"{index++}.Multiplication");
+            Console.WriteLine($"{index}.Division");
+        }
+
+        static void welcome()
+        {
+            Console.WriteLine("Hello, What's your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"HELLO {name}, Welcome to the game!!");
+        }
+    }
+}
